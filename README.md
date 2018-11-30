37b861b9c05ac4f2247fa2c914bf0d9c89b88a77
AI-B166ER <newripper666@gmail.com>
2018-11-29 23:21:12 -0200

    Updates site

---
+++ README.md
@@ -0,0 +1,8 @@
+Ramon de Guimarães Apolinario
+
+Umbler
+http://oldschoolgarage-tk.umbler.net
+
+Github
+https://ai-b166er.github.io/
+
--- js/validacao.js
+++ js/validacao.js
@@ -3,14 +3,12 @@ function validaForm () {
 	var nome  = document.forms["contatoForm"]["inpNome"].value;
 	var email  = document.forms["contatoForm"]["inpEmail"].value;
 	var assunto = document.forms["contatoForm"]["opcoes"].value;
-	var termos;
-
+	
 	if (nome == "" || email == "" || assunto == "*Assunto"){
 		document.getElementById("aviso").innerHTML = "Os campos marcados com asterisco são obrigatorios!";
 		return false;
 	} else if (document.forms["contatoForm"]["boxTermos"].checked == false) {
 		alert("Aceite os termos!");
-		return false;
 	}
 }
 
